52791aa6ea17ac6989e43c307b9c52d7cc4e57ee
Rob Wymer <wymer1120@gmail.com>
2017-06-30 12:02:16 -0400

    First Commit

---
+++ Payments/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Alex Wymer  on 2017-06-30.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+        
+        
+    }
+    return 0;
+}
